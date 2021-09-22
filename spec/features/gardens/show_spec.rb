40577f9874f48da618ed8e601d74b359a39b6b3e
Erin Quinn <equinn125@gmail.com>
2021-09-22 11:09:39 -0600

    complete user story 3

--- spec/features/gardens/show_spec.rb
+++ spec/features/gardens/show_spec.rb
@@ -5,19 +5,18 @@ RSpec.describe 'the garden show page' do
     @garden = Garden.create!(name: 'Erins Garden', organic: true)
     @plot_1 = @garden.plots.create!(number: 1, size: "small", direction: 'east')
     @plot_2 = @garden.plots.create!(number: 2, size: "medium", direction: 'west')
-    @plant_1 = @plot_1.plants.create!(name: 'Bell Pepper', description: 'Red bell pepper', days_to_harvest: 30)
+    @plant_1 = @plot_1.plants.create!(name: 'Bell Pepper', description: 'Red bell pepper', days_to_harvest: 300)
     @plant_2 = @plot_1.plants.create!(name: 'Beet', description: 'Beet Root', days_to_harvest: 70)
-    @plant_3 = @plot_2.plants.create!(name: 'Carrot', description: 'Purple Carrot', days_to_harvest: 10)
+    @plant_3 = @plot_2.plants.create!(name: 'Carrot', description: 'Purple Carrot', days_to_harvest: 110)
     @plant_4 = @plot_2.plants.create!(name: 'Kale', description: 'Dino Kale', days_to_harvest: 60)
   end
 
-  it 'shows the garden attribute all its plants and plots' do
+  it 'shows the garden attribute all plants the garden has with less than 100 days to harvest' do
     visit "/gardens/#{@garden.id}"
-    save_and_open_page
     expect(page).to have_content(@garden.name)
-    expect(page).to have_content(@plant_1.name)
+    expect(page).to_not have_content(@plant_1.name)
     expect(page).to have_content(@plant_2.name)
-    expect(page).to have_content(@plant_3.name)
+    expect(page).to_not have_content(@plant_3.name)
     expect(page).to have_content(@plant_4.name)
   end
 end
